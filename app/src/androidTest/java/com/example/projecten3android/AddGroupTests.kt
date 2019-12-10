b70d8fe92d06c10e41dde2ee73a244a4533c83db
Sofie Seru <sofie.seru@student.hogent.be>
2019-12-10 13:11:59 +0100

    test: filter op naam

--- app/src/androidTest/java/com/example/projecten3android/AddGroupTests.kt
+++ app/src/androidTest/java/com/example/projecten3android/AddGroupTests.kt
@@ -9,7 +9,6 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.example.projecten3android.R
 import org.hamcrest.Matchers.allOf
