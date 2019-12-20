e3a5bbfed585f122853e8d27a3d9b1065d88ed41
Keelan Savat <keelan@live.be>
2019-12-20 16:11:51 +0100

    [fix]: fixed status label in shopping bag

--- app/src/main/java/com/klimaatmobiel/ui/viewModels/WebshopViewModel.kt
+++ app/src/main/java/com/klimaatmobiel/ui/viewModels/WebshopViewModel.kt
@@ -300,6 +300,7 @@ class WebshopViewModel(group: Group, private val repository: KlimaatmobielReposi
                 val newOrder = removeAllOrdersDefered.await()
 
                 _group.value!!.order.orderItems.removeAll(_group.value!!.order.orderItems)
+                _group.value!!.order.submitted = false
 
                 // trigger verandering in winkelmandje
                 _group.value = _group.value
